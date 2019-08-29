f7e2e79ca5fd1e76751e4514be0de323e61b21e3
Ayomide Jones <ayormeday@gmail.com>
2019-08-29 11:53:28 +0100

    ability to populate fields in party page, also upload multiple images in party mood

--- party.js
+++ party.js
@@ -1,11 +1,19 @@
 let shoppingList = [];
 let todoList =[];
+let partyList = [];
 let addShoppingItemBtn = document.getElementById("add-shopping-item");
 let addTodoItemBtn = document.getElementById("add-todo-item");
 let addTodoInput = document.getElementById("to-do");
 let addShoplistInput = document.getElementById("shop-list");
 let shoppingListElement = document.getElementById("shopping-list-ul");
 let todoListElement = document.getElementById("todo");
+let partyListElement = document.getElementById("test");
+let addPartyDetails = document.getElementById("add-details");
+let firstFormDetail = document.getElementById("guests");
+let secondFormDetail = document.getElementById("date");
+let thirdFormDetail = document.getElementById("theme");
+let fourthFormDetail = document.getElementById("budget");
+
 
 
 
@@ -15,9 +23,15 @@ function addToShoppingList (shoppingItem){
 }
 
 function addToTodoList (todoItem){
-    todoList.unshift(todoItem);
-    
+    todoList.unshift(todoItem);   
 }
+
+function addPartyList (partyItem){
+    partyList.unshift(partyItem);
+}
+
+
+
 
 function generateListItem (value) {
     let listItem = document.createElement("li");
@@ -49,3 +63,15 @@ addTodoItemBtn.addEventListener ("click", event => {
     todoListElement.appendChild(node);
 
 })
+
+addPartyDetails.addEventListener("click", event =>{
+    event.preventDefault();
+    let value1 = firstFormDetail.value;
+    let value2 = secondFormDetail.value;
+    let value3 = thirdFormDetail.value;
+    let value4 = fourthFormDetail.value;
+    let data = JSON.stringify({name: value1, date: value2, theme: value3, budget: value4})
+    
+    alert(data)
+    console.log(value1,value2,value3,value4);
+});
